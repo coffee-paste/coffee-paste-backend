a1a89a962fe78609522c6b1428ff36da08f9bb7c
Haim Kastner <haim.kastner@gmail.com>
2021-04-17 21:31:20 +0300

    Add app security

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,26 @@ import { app } from "./app";
 import { logger } from './core';
 import { handleChannels } from './routes';
 
+const welcomeMessage = `
+_  _  _ _______        _______  _____  _______ _______ _______  _____ 
+|  |  | |______ |      |       |     | |  |  | |______    |    |     |
+|__|__| |______ |_____ |_____  |_____| |  |  | |______    |    |_____|
+                                                                      
+                 .d888 .d888                                              888            
+                d88P" d88P"                                               888            
+                888   888                                                 888            
+ .d8888b .d88b. 888888888888 .d88b.  .d88b.       88888b.  8888b. .d8888b 888888 .d88b.  
+d88P"   d88""88b888   888   d8P  Y8bd8P  Y8b      888 "88b    "88b88K     888   d8P  Y8b 
+888     888  888888   888   8888888888888888888888888  888.d888888"Y8888b.888   88888888 
+Y88b.   Y88..88P888   888   Y8b.    Y8b.          888 d88P888  888     X88Y88b. Y8b.     
+ "Y8888P "Y88P" 888   888    "Y8888  "Y8888       88888P" "Y888888 88888P' "Y888 "Y8888  
+                                                  888                                    
+                                                  888                                    
+                                                  888                                                                                                                                 
+`;
+// tslint:disable-next-line: no-console
+console.log('\x1b[34m', welcomeMessage, '\x1b[0m');
+
 const port = process.env.PORT || 3000;
 
 const server: any = http.createServer(app).listen(port, () => {
